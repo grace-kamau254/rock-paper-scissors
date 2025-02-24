e7db565d40ceaead638c8522ed25ebb515862d46
grace-kamau254 <kamaugraciie@gmail.com>
2025-02-24 21:37:38 +0300

    added javascript

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const computerChoiceDisplay=document.getElementById('computer-choice')
+const userChoiceDisplay=document.getElementById('user-choice')
+const resultDisplay=document.getElementById('result')
+const possibleResult=document.querySelectorAll('button')
+let userChoice
+
+possibleChoices.forEach(possibleChoice=>.addEventListener('click',(e)=>{userChoice=e.target.id user}))
